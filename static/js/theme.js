9f1c3e46f35c851b42a299ce7baf806ec50c8a02
pgersl <pgersl05@gmail.com>
2023-09-17 16:22:31 +0200

    theme button bug fix

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,28 +1,33 @@
 function setTheme(theme) {
-    const root = document.documentElement;
-    root.setAttribute('data-theme', theme);
-  
-    localStorage.setItem('theme', theme);
-  }
-  
-  function toggleTheme() {
-    const currentTheme = localStorage.getItem('theme') || 'light';
-    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
-  
-    setTheme(newTheme);
-  }
-  
-  const storedTheme = localStorage.getItem('theme');
-  if (storedTheme) {
-    setTheme(storedTheme);
+  const root = document.documentElement;
+  root.setAttribute('data-theme', theme);
+
+  localStorage.setItem('theme', theme);
+  const themeIcon = document.querySelector('.theme-toggle-btn-icon');
+  if (theme === 'light') {
+    themeIcon.classList.remove('fa-moon');
+    themeIcon.classList.add('fa-sun');
   } else {
-    setTheme('light');
+    themeIcon.classList.remove('fa-sun');
+    themeIcon.classList.add('fa-moon');
   }
-  
+}
+
+function toggleTheme() {    
+  const currentTheme = localStorage.getItem('theme') || 'light';
+  const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+  setTheme(newTheme);
+}
+
+const storedTheme = localStorage.getItem('theme');
+if (storedTheme) {
+  setTheme(storedTheme);
+} else {
+  setTheme('light');
+}
+
 const themeToggleBtn = document.getElementById('theme-toggle-btn');
-const themeIcon = document.querySelector('.theme-toggle-btn-icon');
 themeToggleBtn.addEventListener('click', () => {
-  toggleTheme()
-  themeIcon.classList.toggle('fa-sun')
-  themeIcon.classList.toggle('fa-moon')
+toggleTheme()
 });
