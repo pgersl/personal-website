dc73e547c3b3098ff7a88463a22c1b8ecb14c0f4
Petr Geršl <pgersl05@gmail.com>
2024-02-17 09:07:17 +0100

    minor design fixes

--- static/js/nav.js
+++
@@ -1,9 +0,0 @@
-const navToggleButton = document.getElementById('nav-toggle')
-const navbarContainer = document.querySelector('nav')
-const toggleIcon = document.getElementById('nav-toggle-icon')
-
-navToggleButton.addEventListener('click', () => {
-    navbarContainer.classList.toggle('toggled');
-    toggleIcon.classList.toggle('fa-bars');
-    toggleIcon.classList.toggle('fa-xmark');
-})
